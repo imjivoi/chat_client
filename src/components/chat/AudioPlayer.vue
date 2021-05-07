a82195ad97f39ba75d2a4bce02aee5a0afe4b493
jivoi <viat001@mail.ru>
2021-05-06 21:26:19 -0300

    audio player

--- src/components/chat/AudioPlayer.vue
+++ src/components/chat/AudioPlayer.vue
@@ -6,14 +6,17 @@
       <el-button icon="el-icon-video-pause" type="text" circle @click="pause"
                  v-else></el-button>
     </div>
-    <div class="timeline"></div>
-    <div class="time">{{currentTime}}</div>
+    <input @change="setCurrentTime" type="range" ref="input"
+           v-model="currentTime"
+           :max="audio.duration ?? 0">
+    <div class="current-time">{{ formatTime(currentTime) }}</div>
+    <div class="duration-time" v-if="isShowDuration">
+      {{ formatTime(audio.duration) }}
+    </div>
   </div>
 </template>
 
 <script>
-//todo:закончить
-
 
 export default {
 
@@ -26,51 +29,88 @@ export default {
   },
   data: () => ({
     audio: null,
-    isPaused:true
+    isPaused: true,
+    currentTime: 0,
+    isShowDuration: false
   }),
-  computed:{
-    currentTime(){
-      const time=this.audio.currentTime
+
+  methods: {
+    formatTime(time) {
+      if (time ===Infinity) return ''
       let seconds = parseInt(time);
       let minutes = parseInt(seconds / 60);
       seconds -= minutes * 60;
-      return `${minutes} : ${seconds}`
-    }
-  },
-
-  methods:{
-    play(){
-      this.audio.play()
-      this.isPaused=false
+      return `${minutes < 10 ? '0' + minutes : minutes} : ${seconds < 10 ? '0' + seconds : seconds}`
 
     },
-    pause(){
-      this.audio.pause()
-      this.isPaused=true
+    play() {
+      this.audio.play()
+      this.isPaused = false
 
+    },
+    pause() {
+      this.audio.pause()
+      this.isPaused = true
+
+    },
+    setCurrentTime() {
+      this.audio.currentTime = this.currentTime
     }
   },
   mounted() {
     this.audio = new Audio(this.src)
+    this.audio.addEventListener('loadeddata', () => {
+      this.isShowDuration = true
+    });
+    this.audio.addEventListener('timeupdate', () => {
+      this.currentTime = this.audio.currentTime
+    });
   },
-  watch:{
-    audio:function(){
-      console.log(this.audio.paused)
+  watch: {
+    currentTime: function () {
+      if (this.currentTime === this.audio.duration) {
+        this.isPaused = true
+      }
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
+.btn{
+  margin: 0 12px 0 0;
+}
+.el-button{
+  font-size: 18px;
+}
 .audio-player {
   width: 250px;
   height: 50px;
   display: flex;
-  justify-content: space-between;
   align-items: center;
+  position: relative;
+
+  input {
+    width: 100%;
+  }
 }
-.timeline{
+
+.timeline {
   width: 80%;
   height: 1px;
-  background-color: #2a8bf247;}
+  background-color: #2a8bf247;
+}
+
+.current-time, .duration-time {
+  position: absolute;
+  left: 40px;
+  bottom: 0;
+  color: #2a8bf2;
+  font-size: 12px;
+}
+
+.duration-time {
+  left: auto;
+  right: 15px;
+}
 </style>
