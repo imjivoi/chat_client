9abaa3e213a525b1f274b15c39c4945bb93e2ec8
jivoi <viat001@mail.ru>
2021-05-30 10:39:40 -0300

    update app config

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,4 +1,5 @@
 export default {
   apiUrl: process.env.NODE_ENV === 'development' ? process.env.VUE_APP_BASE_URL : '/api',
-  socketUrl: process.env.NODE_ENV === 'development' ? process.env.VUE_APP_SOCKET_URL : '',
+  socketUrl:
+    process.env.NODE_ENV === 'development' ? process.env.VUE_APP_SOCKET_URL || '127.0.0.1:80' : '',
 };
