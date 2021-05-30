60b20bae7da23bafe28233f976713424446742ca
jivoi <viat001@mail.ru>
2021-05-30 09:45:47 -0300

    add prefix, update ci/cd

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,4 +1,4 @@
 export default {
-  apiUrl: process.env.VUE_APP_BASE_URL || 'http://127.0.0.1:4000',
+  apiUrl: process.env.NODE_ENV === 'development' ? process.env.VUE_APP_BASE_URL : '',
   socketUrl: process.env.VUE_APP_SOCKET_URL || '127.0.0.1:80',
-}
+};
